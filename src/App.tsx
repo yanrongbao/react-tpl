5e6a3f642810a9479aaddacc9d4325e178f1d776
yanrongbao1566 <yanrongbao1566@163.com>
2023-04-15 16:11:41 +0800

    feat: add eslint no warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useEffect, useState } from 'react';
-
-import './App.css';
-import logo from './logo.svg';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
